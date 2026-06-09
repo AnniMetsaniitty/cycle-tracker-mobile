c4778a53b6152ce6203712bc2d91ca4087bef368
Anni Metsäniitty <anni.metsaniitty@outlook.com>
2026-06-09 08:25:57 +0300

    style(register): polish layout, spacing, and implement intended colors

--- app/register.tsx
+++ app/register.tsx
@@ -96,14 +96,19 @@ const styles = StyleSheet.create({
     padding: 24,
     justifyContent: "center",
     gap: 12,
-    backgroundColor: "#ffffff",
+    backgroundColor: "#ffe4f0",
   },
   title: {
+    fontFamily: "Montserrat",
     fontSize: 24,
-    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: -1,
+    // fontWeight: "bold",
   },
   description: {
     fontSize: 16,
+    fontFamily: "Inter",
+    letterSpacing: 0.5,
     color: "#444444",
     marginBottom: 8,
   },
@@ -114,13 +119,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 10,
     fontSize: 16,
+    fontFamily: "Inter",
+    letterSpacing: 0.5,
     backgroundColor: "#f8f8f8",
   },
   button: {
     alignItems: "center",
-    backgroundColor: "#2563eb",
+    backgroundColor: "#a63a79",
     borderRadius: 8,
     paddingVertical: 12,
+    
   },
   buttonPressed: {
     opacity: 0.85,
@@ -131,7 +139,9 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "#ffffff",
     fontSize: 16,
-    fontWeight: "600",
+    //fontWeight: "600",
+    fontFamily: "Inter",
+    letterSpacing: 0.5,
   },
   errorText: {
     color: "#b00020",
@@ -139,8 +149,9 @@ const styles = StyleSheet.create({
   },
   link: {
     fontSize: 16,
+    fontFamily: "Inter",
     textDecorationLine: "underline",
-    color: "#2563eb",
+    color: "#2496cd",
     marginTop: 4,
   },
 });
